e645c6862cedf5402b924e48d6f0ee56c0404edb
Hugo Toledo <hugotoledo@gmail.com>
2014-08-17 19:18:35 -0400

    initial changes to cachematrix.R
    added initial comments as requested
    code and comments for cacheSolve remain to be added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R is the program we create for our Programming Assignment 2
+## it serves to teach us about using functions as arguments and storing data in another context or frame
+## it has two functions, makeCacheMatrix() and cacheSolve()
 
-## Write a short comment describing this function
+
+## makeCacheMatrix holds the previously calculated value, if any, and the functions to check for, save and return it
 
 makeCacheMatrix <- function(x = matrix()) {
 
